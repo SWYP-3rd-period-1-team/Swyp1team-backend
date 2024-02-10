4dfb435936b1c897c85fa6e244f905537be01d51
ulsandonghun <donghoon141@naver.com>
2024-02-10 19:50:00 +0900

    [init] : 개발 환경 세팅

---
+++ src/main/java/com/swig/zigzzang/ZigzzangApplication.java
@@ -0,0 +1,13 @@
+package com.swig.zigzzang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigzzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZigzzangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swig/zigzzang/ZigzzangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swig.zigzzang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigzzangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
