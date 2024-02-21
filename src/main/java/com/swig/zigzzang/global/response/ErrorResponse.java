9425eec4a493a89f3fefe31fe49989e9957a1549
ulsandonghun <donghoon141@naver.com>
2024-02-21 12:57:47 +0900

    [fix] : ErrorResponse 형식 수정

--- src/main/java/com/swig/zigzzang/global/response/ErrorResponse.java
+++ src/main/java/com/swig/zigzzang/global/response/ErrorResponse.java
@@ -3,17 +3,17 @@ package com.swig.zigzzang.global.response;
 import com.swig.zigzzang.global.exception.HttpExceptionCode;
 import org.springframework.http.HttpStatus;
 
-public record ErrorResponse(HttpStatus errCode, String message) {
+public record ErrorResponse(String success, String message) {
     public static ErrorResponse from(HttpExceptionCode exceptionCode) {
-        return new ErrorResponse(exceptionCode.getHttpStatus(), exceptionCode.getMessage());
+        return new ErrorResponse("false", exceptionCode.getMessage());
     }
 
     public static ErrorResponse from(HttpExceptionCode exceptionCode, String errorMessage) {
-        return new ErrorResponse(exceptionCode.getHttpStatus(), errorMessage);
+        return new ErrorResponse("false", errorMessage);
     }
 
     public static ErrorResponse from(HttpStatus httpStatus, String errorMessage) {
-        return new ErrorResponse(httpStatus, errorMessage);
+        return new ErrorResponse("false", errorMessage);
     }
 
 }
