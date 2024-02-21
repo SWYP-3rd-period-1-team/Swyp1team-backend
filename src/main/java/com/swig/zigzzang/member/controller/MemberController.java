a29767a591cf14b41f7f97bc90757354b27074f3
ulsandonghun <donghoon141@naver.com>
2024-02-21 12:46:29 +0900

    [fix] : 회원가입 기능 BaseResponse 적용

--- src/main/java/com/swig/zigzzang/member/controller/MemberController.java
+++ src/main/java/com/swig/zigzzang/member/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.swig.zigzzang.member.controller;
 
+import com.swig.zigzzang.global.response.HttpResponse;
 import com.swig.zigzzang.member.domain.Member;
 import com.swig.zigzzang.member.dto.MemberJoinRequest;
 import com.swig.zigzzang.member.dto.MemberJoinResponse;
 import com.swig.zigzzang.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +21,11 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<MemberJoinResponse> join(@Valid @RequestBody MemberJoinRequest memberJoinRequest) {
+    @Operation(summary = "회원가입", description = "회원가입을 실행합니다.")
+
+    public HttpResponse<MemberJoinResponse> join(@Valid @RequestBody MemberJoinRequest memberJoinRequest) {
         Member savedmember = memberService.save(memberJoinRequest);
-        return ResponseEntity.ok(
+        return HttpResponse.okBuild(
                 MemberJoinResponse.of(savedmember)
         );
     }
