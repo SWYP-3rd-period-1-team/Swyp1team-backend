d14a88025ad4c4278a12ba6053d599b3deac07d0
ulsandonghun <donghoon141@naver.com>
2024-02-25 01:03:27 +0900

    [feat] : CustomExceptinoCode 추가

--- src/main/java/com/swig/zigzzang/global/exception/HttpExceptionCode.java
+++ src/main/java/com/swig/zigzzang/global/exception/HttpExceptionCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum HttpExceptionCode {
     UNABLE_TO_SEND_EMAIL(HttpStatus.BAD_REQUEST, "이메일 전송에 실패했습니다."),
-    MEMBER_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 회원입니다.");
+    MEMBER_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
+    INVALID_ARGUMENT(HttpStatus.BAD_REQUEST,  "올바르지 않은 값이 전달되었습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+
+
 
     private final HttpStatus httpStatus;
     private final String message;
