19578fc7cf761341306fa4e97928143e67205018
ulsandonghun <donghoon141@naver.com>
2024-03-22 17:54:06 +0900

    [fix] : 수면시간 저장시 기존 저장값 충돌 애러 해결

--- src/main/java/com/swig/zigzzang/calender/repository/SleepSchedule/SleepScheduleRepository.java
+++ src/main/java/com/swig/zigzzang/calender/repository/SleepSchedule/SleepScheduleRepository.java
@@ -1,10 +1,13 @@
 package com.swig.zigzzang.calender.repository.SleepSchedule;
 
+import com.swig.zigzzang.calender.domain.Calender;
 import com.swig.zigzzang.calender.domain.SleepSchedule;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SleepScheduleRepository extends JpaRepository<SleepSchedule, Long> {
+    Optional<SleepSchedule> findByCalender(Calender calender);
 
 }
